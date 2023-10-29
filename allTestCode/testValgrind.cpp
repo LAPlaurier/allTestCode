76517c1efb64c11a6e56469107c8ac003b517be3
LAPlaurier <sunl21587@gmail.com>
2023-10-29 11:38:27 +0800

    valgrind

--- allTestCode/testValgrind.cpp
+++ allTestCode/testValgrind.cpp
@@ -24,6 +24,8 @@ int main(void) {
 // 内存重复释放
 // valgrind --tool=memcheck --leck-check=full ./testValgrind
 // ==> Invalid free() / delete / delete[] / realloc()
+
+/*
 #include <stdio.h>
   
 int main(int argc, char **argv) {
@@ -42,6 +44,7 @@ int main(int argc, char **argv) {
 
     return 0;
 }
+*/
 
 
 // 内存释放后读写
@@ -93,3 +96,36 @@ int main(int argc, char **argv) {
 */
 
 
+// 多线程数据竞争
+// valgrind --tool=helgrind ./testValgrind
+// ==> Possible data race during read of size 4 at 0x40403C by thread #1
+// ==4037== Locks held: none
+/*
+#include <pthread.h>
+
+int var = 0;
+
+void* child_fn ( void* arg ) {
+   var++;
+   return NULL;
+}
+
+int main ( void ) {
+   pthread_t child;
+   pthread_create(&child, NULL, child_fn, NULL);
+   var++;
+   pthread_join(child, NULL);
+   return 0;
+}
+*/
+
+
+// 不正确的匹配
+// valgrind --tool=memcheck --leak-check=full ./testValgrind
+// ==> Mismatched free() / delete / delete []
+#include <iostream>
+int main() {
+    int *a = new int(10);
+    free(a);
+    return 0;
+}
